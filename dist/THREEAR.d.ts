c28bcdfc3970eee2f40ca89038200d5f61bf3a7d
Kubiak <joseph.kubiak@gmail.com>
2019-09-29 18:51:29 -0700

    Tryingt of fix artoolkit not defined error when using multimarkers.

--- dist/THREEAR.d.ts
+++ dist/THREEAR.d.ts
@@ -4,8 +4,6 @@ import BarcodeMarker from "./BarcodeMarker";
 import MultiMarker from "./MultiMarker";
 import { Controller, ControllerParameters } from "./Controller";
 declare let controller: Controller;
-declare function initialize(
-	parameters: Partial<ControllerParameters>
-): Promise<any>;
+declare function initialize(parameters: Partial<ControllerParameters>): Promise<any>;
 export declare type Controller = typeof controller;
 export { Source, initialize, PatternMarker, BarcodeMarker, MultiMarker };
