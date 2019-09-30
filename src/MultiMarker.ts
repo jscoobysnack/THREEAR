93f18c16567cb06f48a8ae1e7746c6e0c0fa6e80
Kubiak <joseph.kubiak@gmail.com>
2019-09-29 17:41:47 -0700

    Adding multimarker type.

--- src/MultiMarker.ts
+++ src/MultiMarker.ts
@@ -2,7 +2,7 @@ import { Object3D } from "three";
 import { BaseMarker } from "./BaseMarker";
 
 /**
- * PatternMarker takes a given image (as a URL) and use this as the pattern to detect to place
+ * MultiMarker takes a given image (as a URL) and use this as the pattern to detect to place
  * a given marker from three.js, in this case an given Object3D
  * @param parameters parameters for determining things the pattern URL and minimum confidence
  */
